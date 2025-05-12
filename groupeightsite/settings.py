5732214f44b68a65fe77f61b177c2a7b4b965357
THOTW <thotw@192.168.0.101>
2025-05-12 17:04:50 +0800

    part3

--- groupeightsite/settings.py
+++ groupeightsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crud.apps.CrudConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,11 +75,14 @@ WSGI_APPLICATION = 'groupeightsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'Localhost',
+        'NAME': 'groupeight.db',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '',
     }
-}
+
 
 
 # Password validation
