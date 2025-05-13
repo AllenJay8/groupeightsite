878d3a457f822061c3965b83bfa4a77c9b35325f
AllenJay8 <alaher@filamer.edu.ph>
2025-05-13 16:32:59 +0800

    part4 "Templates > gender"

--- crud/views.py
+++ crud/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
+
+def test(request):
+    return render(request, 'layout/base.html')
--- groupeightsite/urls.py
+++ groupeightsite/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('', include('crud.urls')),
     path('admin/', admin.site.urls),
 ]
+ 
