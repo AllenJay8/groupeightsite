09745194c8eaf656fa89a4b2762dc94284ff403b
AllenJay8 <alaher@filamer.edu.ph>
2025-05-16 09:25:36 +0800

    Tulog Maayo

--- crud/urls.py
+++ crud/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.gender_list, name='gender_list'),
-    path('gender/add/', views.add_gender, name='add_gender'),
-    path('gender/edit/<int:id>/', views.edit_gender, name='edit_gender'),
-    path('gender/delete/<int:id>/', views.delete_gender, name='delete_gender'),
+    path('gender/add/', views.add_gender),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,33 +1,17 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Genders
 
-def gender_list(request):
-    genders = Genders.objects.all()
-    return render(request, 'gender/GendersList.html', {'genders': genders})
+# Create your views here.
 
 def add_gender(request):
-    if request.method == 'POST':
-        gender_name = request.POST.get('gender')
-        if gender_name:
-            Genders.objects.create(gender=gender_name)
-            messages.success(request, 'Gender added successfully!')
-            return redirect('gender_list')
-    return render(request, 'gender/AddGender.html')
-
-def edit_gender(request, id):
-    gender = get_object_or_404(Genders, id=id)
-    if request.method == 'POST':
-        gender_name = request.POST.get('gender')
-        if gender_name:
-            gender.gender = gender_name
-            gender.save()
-            messages.success(request, 'Gender updated successfully!')
-            return redirect('gender_list')
-    return render(request, 'gender/EditGender.html', {'gender': gender})
-
-def delete_gender(request, id):
-    gender = get_object_or_404(Genders, id=id)
-    gender.delete()
-    messages.success(request, 'Gender deleted successfully!')
-    return redirect('gender_list')
+    try:
+        if request.method == 'POST':
+            gender = request.POSTg.get('gender')
+            
+            Genders.objects.create(gender=gender).save()
+            return HttpResponse('Gender added successfully!')
+        else:
+            return render(request, 'gender/AddGender.html')
+    except Exception as e:
+       return HttpResponse(f'Error occured during add gender: {e}')
